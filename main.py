4afd46b5f6fde8f9249e948892cf490e36397149
vivek <vivekpatwari38@gmail.com>
2025-05-21 20:58:49 +0530

    added requirements.txt file

--- main.py
+++ main.py
@@ -171,4 +171,5 @@ def get_stored_data(key):
         return jsonify({"error": f"Error retrieving data: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
